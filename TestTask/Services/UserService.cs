9cff45d3f6a29173457420ca0af59adf5ad5b2a3
m3mph17 <ez4genius@gmail.com>
2023-10-02 18:23:02 +0300

    User service with code which is getting user with max orders and all users which are inactive

---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    internal class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _db;
+        public UserService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var userWithMaxOrders = await _db.Users
+                     .Include(u => u.Orders)
+                     .OrderByDescending(u => u.Orders.Count)
+                     .FirstOrDefaultAsync();
+
+            if (userWithMaxOrders == null)
+            {
+                return null;
+            }
+
+            return userWithMaxOrders;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var inactiveUsers = await _db.Users
+                .Where(u => u.Status == Enums.UserStatus.Inactive)
+                .ToListAsync();
+
+            return inactiveUsers;
+        }
+    }
+}
